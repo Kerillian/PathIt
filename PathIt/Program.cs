9a92501348f7e51c39b91dc911afd5d1f09e5aa2
Kerillian <49354481+Kerillian@users.noreply.github.com>
2021-04-07 17:57:33 -0400

    Init

---
+++ PathIt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PathIt
+{
+    public class Program
+    {
+        private const string PATH = "PATH";
+
+        private static int Main(string[] args)
+        {
+            string path = Path.GetFullPath(args.Length > 0 ? args[0] : Directory.GetCurrentDirectory());
+
+            if (Directory.Exists(path))
+            {
+                List<string> pathsInPATH = Environment.GetEnvironmentVariable(PATH, EnvironmentVariableTarget.User)?.Split(';').ToList();
+
+                if (pathsInPATH == null)
+                {
+                    return 1;
+                }
+                
+                if (!pathsInPATH.Contains(path))
+                {
+                    pathsInPATH.Add(path);
+                    
+                    Environment.SetEnvironmentVariable(PATH, string.Join(";", pathsInPATH), EnvironmentVariableTarget.User);
+                    Console.WriteLine($"Added \"{path}\" to PATH.");
+                }
+                else
+                {
+                    pathsInPATH.Remove(path);
+                    
+                    Environment.SetEnvironmentVariable(PATH, string.Join(";", pathsInPATH), EnvironmentVariableTarget.User);
+                    Console.WriteLine($"Removed \"{path}\" from PATH.");
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("[Error] Input path is not a directory.");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
